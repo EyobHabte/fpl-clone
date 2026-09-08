80089b0e2fa1f9e3bcdd0146a177013edd8d36e0
EyobHabte <eyobhabte16@gmail.com>
2026-09-08 08:22:47 +0300

    Remove SMS feature, upgrade Next.js to 14.2.35

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -8,12 +8,5 @@ export default async function SettingsPage() {
   const user = await getCurrentUser();
   if (!user) redirect('/login');
 
-  return (
-    <SettingsClient
-      email={user.email}
-      teamName={user.teamName}
-      phone={user.phone}
-      phoneVerified={!!user.phoneVerified}
-    />
-  );
+  return <SettingsClient email={user.email} teamName={user.teamName} />;
 }
